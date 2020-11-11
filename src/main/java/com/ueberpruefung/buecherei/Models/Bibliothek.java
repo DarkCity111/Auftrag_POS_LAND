8d67879769ab3dd2f70a8667e13eb27e9fbdb78f
Marco <fadum.marco@gmail.com>
2020-11-11 14:46:16 +0100

    Entitäten implementiert

--- src/main/java/com/ueberpruefung/buecherei/Models/Bibliothek.java
+++ src/main/java/com/ueberpruefung/buecherei/Models/Bibliothek.java
@@ -1,7 +1,21 @@
 package com.ueberpruefung.buecherei.Models;
 
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Bibliothek {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotNull
+    private String bibliothekname;
+
+    @OneToMany
+    private List<Buch> buecher;
 
 }
--- src/main/java/com/ueberpruefung/buecherei/Models/Buch.java
+++ src/main/java/com/ueberpruefung/buecherei/Models/Buch.java
@@ -1,10 +1,22 @@
 package com.ueberpruefung.buecherei.Models;
 
-import javax.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Buch {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotNull
+    private String buchname;
 
+    @ManyToOne
+    @JsonIgnore
+    private Bibliothek bibliothek;
 }
